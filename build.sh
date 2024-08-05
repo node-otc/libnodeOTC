f54f3f45c16cf725d0bb5718a5851121b5a03f6a
Amber-Sophia Schröck <zweiter.vorsitz.sec@im-nrw.de>
2024-08-05 21:15:47 +0200

    switch to c#

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo rm -r build
-mkdir build
-cd build
-
-cmake .. -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_DIR="$(llvm-config --cmakedir)" -GNinja 
-
-ninja -v
--- include/ndbenadetta_capi.hpp
+++
@@ -1,4 +0,0 @@
-#ifndef __BENADETTA_CAPI_OTC_H__
-#define __BENADETTA_CAPI_OTC_H__
-
-#endif
